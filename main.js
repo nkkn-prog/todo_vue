9c4366e763f2251952a3cc28ba533cce0ea31073
安藤直城 <andonaoki@Macbook-pro-8.local>
2023-03-30 18:42:10 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+new Vue ({
+  el: '#app',
+  data: {
+    message: 'Hello World',
+    newTask: '',
+    todo: [
+      {todo_name: "Vue.jsを使ってtodoアプリを作成する", isCompleted: false},
+      {todo_name: "基本情報技術者試験の練習問題を解く", isCompleted: false},
+      {todo_name: "入社先の自己紹介スライド作る", isCompleted: false}
+    ]
+  },
+  methods: {
+    addTask: function(){
+      if (this.newTask == ''){
+        return;
+      }
+      else {
+        this.todo.push({
+        todo_name: this.newTask, isCompleted: false
+        });
+      }
+      this.newTask = '';
+    },
+
+
+  }
+})
